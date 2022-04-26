537505be3b772e1cbf267a1ec0e9e589b8dd43ff
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2022-04-26 15:58:12 +0300

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        fontFamily: 'Quicksand',
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber, cardColor: Colors.white70)
+            .copyWith(secondary: Colors.purple),
+        // iconTheme: const IconThemeData(
+        //   color: Colors.white,
+        //   opacity: 0.5,
+        // ),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.black87),
+          headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.red),
+          button: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.purpleAccent),
+        ),
+        appBarTheme: AppBarTheme(
+          iconTheme: const IconThemeData(color: Colors.white, opacity: 0.8, size: 30),
+          toolbarTextStyle: const TextTheme(
+            headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.red,
+            ),
+          ).bodyText2,
+          titleTextStyle: const TextTheme(
+            headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.purple,
+            ),
+          ).headline6,
+        ),
+      ),
       home: MyHomePage(),
     );
   }
@@ -41,33 +86,37 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter App'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {}),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                color: Colors.amber,
-                width: double.infinity,
-                child: const Card(
-                  color: Colors.deepPurpleAccent,
-                  elevation: 5,
-                  child: Text('Chart'),
-                ),
-              ),
-              const UserTransactions(),
-            ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Personal Expenses'),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.add, color: Theme.of(context).appBarTheme.iconTheme?.color),
+                onPressed: () {}),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                // Container(
+                //   color: Colors.amber,
+                //   width: double.infinity,
+                //   child: const Card(
+                //     color: Colors.deepPurpleAccent,
+                //     elevation: 5,
+                //     child: Text('Chart'),
+                //   ),
+                // ),
+                UserTransactions(),
+              ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add, color: Theme.of(context).appBarTheme.iconTheme?.color),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          tooltip: "press to add new transaction",
+          onPressed: () {},
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,27 +15,30 @@ class TransactionList extends StatelessWidget {
       child: ListView.builder(
         itemBuilder: (ctx, index) {
           return Card(
+            color: Theme.of(context).colorScheme.surface,
             child: Row(
               children: <Widget>[
                 Container(
                   margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
                   decoration: BoxDecoration(
                       border: Border.all(
-                          color: const Color.fromARGB(255, 102, 173, 231),
+                          // color: const Color.fromARGB(255, 102, 173, 231),
+                          color: Theme.of(context).colorScheme.primary,
                           width: 2.0,
                           style: BorderStyle.solid)),
                   padding: const EdgeInsets.all(10),
                   child: Text('¥: ${transactions[index].amount.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 102, 173, 231))),
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                        // color: Color.fromARGB(255, 102, 173, 231)
+                      )),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(transactions[index].title,
-                        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text(transactions[index].title, style: Theme.of(context).textTheme.bodyText2),
                     Text(DateFormat().format(transactions[index].date),
                         style: const TextStyle(fontSize: 14, color: Colors.grey)),
                   ],
